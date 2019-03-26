8632ff01aa7452c2141103d231bb95b62292824c
Jon Lighthall <jon.lighthall@gmail.com>
2019-03-26 16:22:28 -0500

    add test_abs from https://gcc.gnu.org/onlinedocs/gcc-4.4.2/gfortran/ABS.html

---
+++ test_abs.f
@@ -0,0 +1,8 @@
+      program test_abs
+      integer :: i = -1
+      real :: x = -1.e0
+      complex :: z = (-1.e0,0.e0)
+      i = abs(i)
+      x = abs(x)
+      x = abs(z)
+      end program test_abs
