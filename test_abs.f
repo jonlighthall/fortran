fcb601b0cb6593945b97f7072d2ad0f78237925b
Jon Lighthall <jon.lighthall@gmail.com>
2019-03-26 16:24:39 -0500

    add write commands

--- test_abs.f
+++ test_abs.f
@@ -2,7 +2,13 @@
       integer :: i = -1
       real :: x = -1.e0
       complex :: z = (-1.e0,0.e0)
+      write(*,*) 'i = ',i
       i = abs(i)
+      write(*,*) 'abs i = ',i
+      write(*,*) 'x = ',x
       x = abs(x)
+      write(*,*) 'abs x = ',x
+      write(*,*) 'z = ',z
       x = abs(z)
+      write(*,*) 'abs z = ',x
       end program test_abs
