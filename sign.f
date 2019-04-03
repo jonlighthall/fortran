677cc313bf6aac204ba973a6a7759c9a2481abc9
Jon Lighthall <jon.lighthall@gmail.com>
2019-04-03 16:29:13 -0500

    remove header var

--- sign.f
+++ sign.f
@@ -1,6 +1,5 @@
-      PROGRAM  SignControl
-      CHARACTER(LEN=*), PARAMETER :: Heading = "   SS   SP  SS    SP"
-      WRITE(*,"(1X,A)") Heading
+      PROGRAM  sign
+      WRITE(*,"(1X,A)") "   SS   SP  SS    SP"
       DO i = -5, 5
          x = REAL(i)
          write(*,'(sp,2(i5),2(f6.1))')i,i,x,x
