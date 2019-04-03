4227b7c6282e102b1d838128bb7f1667315f3a69
Jon Lighthall <jon.lighthall@gmail.com>
2019-04-03 16:27:00 -0500

    remove implicit, end prog

--- sign.f
+++ sign.f
@@ -1,11 +1,8 @@
       PROGRAM  SignControl
-      IMPLICIT  NONE
-      INTEGER   :: i
-      REAL      :: x
       CHARACTER(LEN=*), PARAMETER :: Heading = "   SS   SP  SS    SP"
       WRITE(*,"(1X,A)") Heading
       DO i = -5, 5
          x = REAL(i)
          write(*,'(sp,2(i5),2(f6.1))')i,i,x,x
       END DO
-      END PROGRAM  SignControl
+      END 
