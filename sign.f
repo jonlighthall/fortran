386f67eb765b8a9384ec2ef068832e507b1ce01b
Jon Lighthall <jon.lighthall@gmail.com>
2019-04-03 16:26:16 -0500

    remove format

--- sign.f
+++ sign.f
@@ -2,13 +2,10 @@
       IMPLICIT  NONE
       INTEGER   :: i
       REAL      :: x
-      CHARACTER(LEN=*), PARAMETER :: Format  = "(1X,SS,I5,SP,I5,SS,F6.1
-     &     ,SP,F6.1)"
-      CHARACTER(LEN=*), PARAMETER :: Heading = "   SS   SP    SS    SP"
-
+      CHARACTER(LEN=*), PARAMETER :: Heading = "   SS   SP  SS    SP"
       WRITE(*,"(1X,A)") Heading
       DO i = -5, 5
          x = REAL(i)
-         WRITE(*,Format)  i, i, x, x
+         write(*,'(sp,2(i5),2(f6.1))')i,i,x,x
       END DO
       END PROGRAM  SignControl
