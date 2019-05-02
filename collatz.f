f29c9ab6ef652269242cfc85ad19c834ff3a51c5
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-02 17:37:03 -0500

    added 16 bit integer, overflow protection (works!)

--- collatz.f
+++ collatz.f
@@ -1,6 +1,6 @@
       program collatz
       implicit none
-      integer n,i,seq
+      integer*16 n,i,seq
       write(*,*)'enter arbitrary positive integer'
       read *,n
       write(*,*)'received input ',n
@@ -10,15 +10,27 @@
          n=seq(n)
          i=i+1
       enddo
-      write(*,*)'i = ',i,n
+      write(*,*)'i = ',i
       end
 
-      integer function seq(n)
-      integer n
+      integer*16 function seq(n)
+      implicit none
+      integer*16 n,seq,nmax
+      nmax=42                   ! 1 bit
+      nmax=10922                ! 2 bit
+      nmax=715827882            ! 4 bit
+      nmax=3074457345618258602  ! 8 bit
+      nmax=56713727820156410577229101238628035242 ! 16 bit
       if (mod(n,2).eq.0) then
-         seq=n/2.
+         seq=n/2
       else
-         seq=3.*n+1.
+         if(n.gt.nmax) then
+            write(*,*)'ERROR: Overflow imminent.'
+            write(*,*)'Maximum hailstone size exceeded',n,3*n+1
+            seq=1
+         else
+            seq=3*n+1
+         endif
       endif
       return
       end
