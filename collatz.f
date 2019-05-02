2560c598e51c66daa817c97ac7beee81d61b266b
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-02 16:19:07 -0500

    add implicit none

--- collatz.f
+++ collatz.f
@@ -1,9 +1,9 @@
       program collatz
-      integer n
+      implicit none
+      integer n,i,seq
       write(*,*)'enter arbitrary positive integer'
       read *,n
       write(*,*)'received input ',n
-      bak=n
       i=0
       do while (n.gt.1)
          write(*,*)'i = ',i,n
@@ -13,7 +13,7 @@
       write(*,*)'i = ',i,n
       end
 
-      real function seq(n)
+      integer function seq(n)
       integer n
       if (mod(n,2).eq.0) then
          seq=n/2.
