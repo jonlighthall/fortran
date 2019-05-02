02069118dfa352e7f6f2bfc354218b18a354f84d
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-02 13:38:39 -0500

    remove comments, add start

--- timedate.f
+++ timedate.f
@@ -1,18 +1,12 @@
       program test_time_and_date
-c      character(8)  :: date
-c      character(10) :: time
-c      character(5)  :: zone
       integer,dimension(8) :: values
-! using keyword arguments
-c      call date_and_time(date,time,zone,values)
-c      call date_and_time(DATE=date,ZONE=zone)
-c      call date_and_time(TIME=time)
-      call date_and_time(VALUES=values)
-c     print '(a,2x,a,2x,a)', date, time, zone
+       call date_and_time(VALUES=values)
       print '(8i5)', values
       write(*,'(a,i0.2,a,i0.2,a,i4)')'the date is ',values(2),'/'
      &     ,values(3),'/',values(1)
       write(*,'(a,i0.2,a,i0.2,a,i0.2,a,i0.3,a,sp,i0.2,a)')'the time is '
      &     ,values(5),':',values(6),':',values(7),'.',values(8),' '
      &     ,values(4)/60,' UTC'
+      start=values(6)*60.+values(7)+values(8)/1000.
+      write(*,*) 'start time is ', start
       end program test_time_and_date
