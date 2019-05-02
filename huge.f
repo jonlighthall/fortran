7083257826041ab80f2c005b643d2545c6c9891c
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-02 16:03:28 -0500

    much faster at finding max

--- huge.f
+++ huge.f
@@ -1,13 +1,25 @@
       program huge
       implicit none
-      integer*1 i, j
+      integer*4 i, j, k
+      real r
       i=1 
       j=0
       do while (i.gt.j)
          j=i
-         i=i+1
+         i=i*2
       enddo
-      write(*,*)i,j
+      write(*,*)i,j,'power'
+      r=log10(real(j))
+      do k=floor(r),0,-1
+         i=j
+         j=i-1
+         do while (i.gt.j)
+            j=i
+            i=i+10**k
+         enddo
+         write(*,*)i,j,k,'decade'
+      enddo
+      write(*,*) 'the largest integer is', j
       end
 
 c     1 byte  127
