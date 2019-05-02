5865f7715a7564e03b7d5abba040bc1448856eb0
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-02 16:12:04 -0500

    working up to 128 bits, remove real, writes

--- huge.f
+++ huge.f
@@ -1,27 +1,25 @@
       program huge
       implicit none
-      integer*4 i, j, k
-      real r
+      integer*16 i, j, k
       i=1 
       j=0
       do while (i.gt.j)
          j=i
          i=i*2
       enddo
-      write(*,*)i,j,'power'
-      r=log10(real(j))
-      do k=floor(r),0,-1
+      do k=floor(log10(real(j))),0,-1
          i=j
          j=i-1
          do while (i.gt.j)
             j=i
             i=i+10**k
          enddo
-         write(*,*)i,j,k,'decade'
       enddo
       write(*,*) 'the largest integer is', j
       end
 
-c     1 byte  127
-c     2 bytes 32767
-c     4 bytes 2147483647
+c      1 byte    8 bits 127
+c      2 bytes  16 bits 32767
+c      4 bytes  32 bits 2147483647
+c      8 bytes  64 bits 9223372036854775807
+c     16 bytes 128 bits 170141183460469231731687303715884105727
