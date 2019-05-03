5d4aac9a6a338205bc850d2c905ac9eefa4d2918
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-03 13:41:00 -0500

    add meaningful var names

--- collatz_loop.f
+++ collatz_loop.f
@@ -1,6 +1,6 @@
       program collatz_loop
       implicit none
-      integer*1 i,j,k,n,nmax,maxstep,nstep
+      integer*2 i,j,k,n,nmax,maxdelay,delay_record,seed,delay
       i=1 
       j=0
       do while (i.gt.j)
@@ -18,37 +18,28 @@
       write(*,*) '          the largest integer is', j
       nmax=(j-1)/3
       write(*,*) 'the largest hailstone integer is', nmax
-      maxstep=0
-      nstep=0
-      do j=0,nmax
-         write(*,*)'seed = ',j
-         n=j
-         i=0
+      maxdelay=0
+      delay_record=0
+      do seed=0,nmax
+         n=seed
+         delay=0
          do while (n.gt.1)
-            write(*,*)'step = ',i,n
             if (mod(n,2).eq.0) then
                n=n/2
             else
                if(n.gt.nmax) then
-                  write(*,*)'ERROR: Overflow imminent.'
-                  write(*,*)'Maximum hailstone size exceeded'
-                  write(*,*)'seed = ',j
-                  write(*,*)'   n = ',n
-                  write(*,*)'nmax = ',nmax
-                  write(*,*)'3n+1 = ',3*n+1
-                  k=3*n+1
-                  write(*,*)'   k = ',k
+                  write(*,*)'ERROR ',n,delay,seed
                   n=1
                else
                   n=3*n+1
                endif
             endif
-            i=i+1
+            delay=delay+1
          enddo
-         if (i>maxstep) then
-            nstep=nstep+1
-            write(*,*)'max = ',nstep,i,j
-            maxstep=i
+         if (delay>maxdelay) then
+            delay_record=delay_record+1
+            write(*,*)'max = ',delay_record,delay,seed
+            maxdelay=delay
          endif
       enddo
       end
