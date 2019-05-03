5738fd081731db0ff342174ed93fc410d8b336e8
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-03 10:00:07 -0500

    make findmax and steps both functions

--- collatz_loop.f
+++ collatz_loop.f
@@ -1,7 +1,24 @@
       program collatz_loop
       implicit none
-      integer*16 n,seq,nmax
+      integer*16 n,seq,nmax,steps,j,findnmax,i
 
+      
+      j=findnmax()
+      n=j
+      nmax=(j-1)/3
+
+      write(*,*) '          the largest integer is', n
+      write(*,*) 'the largest hailstone integer is', nmax
+
+      n=nmax
+
+      i=steps(n,nmax)
+      write(*,*)'i = ',i
+      end
+
+      integer*16 function findnmax()
+      implicit none
+c     find maximum integer size without any a priori knowldege
       integer*16 i, j, k
       i=1 
       j=0
@@ -17,23 +34,22 @@
             i=i+10**k
          enddo
       enddo
-      
-      n=j
-      nmax=(j-1)/3
+      findnmax=j
+      return
+      end
 
-      write(*,*) '          the largest integer is', n
-      write(*,*) 'the largest hailstone integer is', nmax
-
-      n=nmax
-
-      write(*,*)'received input ',n
-      i=0
+      integer*16 function steps(n,nmax)
+      implicit none
+      integer*16 i,n,seq,nmax
+c     write(*,*)'received input ',n
+       i=0
       do while (n.gt.1)
-         write(*,*)'i = ',i,n
+c         write(*,*)'i = ',i,n
          n=seq(n,nmax)
          i=i+1
       enddo
-      write(*,*)'i = ',i
+      steps=i
+      return
       end
 
       integer*16 function seq(n,nmax)
@@ -44,7 +60,7 @@
       else
          if(n.gt.nmax) then
             write(*,*)'ERROR: Overflow imminent.'
-            write(*,*)'Maximum hailstone size exceeded',n,3*n+1
+            write(*,*)'Maximum (odd) hailstone size exceeded',n,3*n+1
             write(*,*)'   n = ',n
             write(*,*)'nmax = ',nmax
             write(*,*)'3n+1 = ',3*n+1
