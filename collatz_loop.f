5e339e853caa5cf420f9244eb16239044324268c
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-03 13:15:06 -0500

    working example with writes

--- collatz_loop.f
+++ collatz_loop.f
@@ -1,7 +1,6 @@
       program collatz_loop
       implicit none
-      integer*16 n,nmax,iter,maxstep,nstep
-      integer*16 i, j, k
+      integer*1 i,j,k,n,nmax,maxstep,nstep
       i=1 
       j=0
       do while (i.gt.j)
@@ -19,24 +18,26 @@
       write(*,*) '          the largest integer is', j
       nmax=(j-1)/3
       write(*,*) 'the largest hailstone integer is', nmax
-      n=nmax
       maxstep=0
       nstep=0
-      do iter=0,1000000
-         n=iter
-c         write(*,*)'received input ',n
+      do j=0,nmax
+         write(*,*)'seed = ',j
+         n=j
          i=0
          do while (n.gt.1)
-c            write(*,*)'i = ',i,n
+            write(*,*)'step = ',i,n
             if (mod(n,2).eq.0) then
                n=n/2
             else
                if(n.gt.nmax) then
                   write(*,*)'ERROR: Overflow imminent.'
-                  write(*,*)'Maximum hailstone size exceeded',n,3*n+1
+                  write(*,*)'Maximum hailstone size exceeded'
+                  write(*,*)'seed = ',j
                   write(*,*)'   n = ',n
                   write(*,*)'nmax = ',nmax
                   write(*,*)'3n+1 = ',3*n+1
+                  k=3*n+1
+                  write(*,*)'   k = ',k
                   n=1
                else
                   n=3*n+1
@@ -46,7 +47,7 @@ c            write(*,*)'i = ',i,n
          enddo
          if (i>maxstep) then
             nstep=nstep+1
-            write(*,*)'i = ',nstep,i,iter
+            write(*,*)'max = ',nstep,i,j
             maxstep=i
          endif
       enddo
