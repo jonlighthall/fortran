6d71461bb9c5220489132978abea9b9b5c6159ba
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-03 12:40:54 -0500

    rollback and rewrite
    remove all functions
    add loop

--- collatz_loop.f
+++ collatz_loop.f
@@ -1,16 +1,6 @@
       program collatz_loop
       implicit none
-      integer*16 nmax,steps,imax
-
-      nmax=(imax()-1)/3
-      write(*,*) 'the largest hailstone integer is', nmax
-
-      write(*,*)'i = ',steps(nmax,nmax)
-      end
-
-      integer*16 function imax()
-      implicit none
-c     find maximum integer size without any a priori knowldege
+      integer*16 n,nmax,iter
       integer*16 i, j, k
       i=1 
       j=0
@@ -26,42 +16,32 @@ c     find maximum integer size without any a priori knowldege
             i=i+10**k
          enddo
       enddo
-      imax=j
       write(*,*) '          the largest integer is', j
-      return
-      end
-
-      integer*16 function steps(n,nmax)
-c     calculate number of steps
-      implicit none
-      integer*16 n,seq,nmax
-c     write(*,*)'received input ',n
-      steps=0
-      do while (n.gt.1)
-c     write(*,*)'steps = ',steps,n
-         n=seq(n,nmax)
-         steps=steps+1
+      nmax=(j-1)/3
+      write(*,*) 'the largest hailstone integer is', nmax
+      n=nmax
+      do iter=0,100
+         n=iter
+         write(*,*)'received input ',n
+         i=0
+         do while (n.gt.1)
+            write(*,*)'i = ',i,n
+            if (mod(n,2).eq.0) then
+               n=n/2
+            else
+               if(n.gt.nmax) then
+                  write(*,*)'ERROR: Overflow imminent.'
+                  write(*,*)'Maximum hailstone size exceeded',n,3*n+1
+                  write(*,*)'   n = ',n
+                  write(*,*)'nmax = ',nmax
+                  write(*,*)'3n+1 = ',3*n+1
+                  n=1
+               else
+                  n=3*n+1
+               endif
+            endif
+            i=i+1
+         enddo
+         write(*,*)'i = ',i
       enddo
-      return
-      end
-
-      integer*16 function seq(n,nmax)
-c     calculate sequence
-      implicit none
-      integer*16 n,seq,nmax
-      if (mod(n,2).eq.0) then
-         seq=n/2
-      else
-         if(n.gt.nmax) then
-            write(*,*)'ERROR: Overflow imminent.'
-            write(*,*)'Maximum (odd) hailstone size exceeded',n,3*n+1
-            write(*,*)'   n = ',n
-            write(*,*)'nmax = ',nmax
-            write(*,*)'3n+1 = ',3*n+1
-            seq=1
-         else
-            seq=3*n+1
-         endif
-      endif
-      return
       end
