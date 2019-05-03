833cab530d80d60ac754720bfed111b14cfc9fd8
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-03 13:49:26 -0500

    working write to file

--- collatz_loop.f
+++ collatz_loop.f
@@ -15,9 +15,12 @@
             i=i+10**k
          enddo
       enddo
+      open(1,file = 'collatz.out',status='unknown',action='write')
       write(*,*) '          the largest integer is', j
+      write(1,*) '          the largest integer is', j
       nmx=(j-1)/3
       write(*,*) 'the largest hailstone integer is', nmx
+      write(1,*) 'the largest hailstone integer is', nmx
       dlymx=0                   ! delay max
       dlyrec=0                  ! delay record
       do sd=0,nmx               ! seed range
@@ -29,6 +32,7 @@
             else
                if(n.gt.nmx) then
                   write(*,*)'ERROR ',n,dly,sd
+                  write(1,*)'ERROR ',n,dly,sd
                   n=1           ! exit loop
                else
                   n=3*n+1
@@ -39,7 +43,9 @@
          if (dly>dlymx) then
             dlyrec=dlyrec+1  
             write(*,*)'mx = ',dlyrec,dly,sd
+            write(1,*)'mx = ',dlyrec,dly,sd
             dlymx=dly
          endif
       enddo
+      close(1)
       end
