187a86442ccac51a8f8e32a59eef7d66d22f8e9d
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-03 09:40:18 -0500

    replace user input with maximum integer

--- collatz_loop.f
+++ collatz_loop.f
@@ -1,39 +1,53 @@
-      program collatz
+      program collatz_loop
       implicit none
-      integer*16 n,i
-      write(*,*)'enter arbitrary positive integer'
-      read *,n
-      in(n)
-      end
+      integer*16 n,seq,nmax
 
-      integer*16 function in(n)
-      implicit none
-      integer*16 n,in,seq
-c      write(*,*)'received input ',n
-      in=0
-      do while (n.gt.1)
-c     write(*,*)'in = ',i,n
-         n=seq(n)
-         in=in+1
+      integer*16 i, j, k
+      i=1 
+      j=0
+      do while (i.gt.j)
+         j=i
+         i=i*2
       enddo
-      write(*,*)'in = ',in
-      return
+      do k=floor(log10(real(j))),0,-1
+         i=j
+         j=i-1
+         do while (i.gt.j)
+            j=i
+            i=i+10**k
+         enddo
+      enddo
+      
+      n=j
+      nmax=(j-1)/3
+
+      write(*,*) '          the largest integer is', n
+      write(*,*) 'the largest hailstone integer is', nmax
+
+      n=nmax
+
+      write(*,*)'received input ',n
+      i=0
+      do while (n.gt.1)
+         write(*,*)'i = ',i,n
+         n=seq(n,nmax)
+         i=i+1
+      enddo
+      write(*,*)'i = ',i
       end
 
-      integer*16 function seq(n)
+      integer*16 function seq(n,nmax)
       implicit none
-      integer*16 n,nmax
-      nmax=42                   ! 1 bit
-      nmax=10922                ! 2 bit
-      nmax=715827882            ! 4 bit
-      nmax=3074457345618258602  ! 8 bit
-      nmax=56713727820156410577229101238628035242 ! 16 bit
+      integer*16 n,seq,nmax
       if (mod(n,2).eq.0) then
          seq=n/2
       else
          if(n.gt.nmax) then
             write(*,*)'ERROR: Overflow imminent.'
             write(*,*)'Maximum hailstone size exceeded',n,3*n+1
+            write(*,*)'   n = ',n
+            write(*,*)'nmax = ',nmax
+            write(*,*)'3n+1 = ',3*n+1
             seq=1
          else
             seq=3*n+1
