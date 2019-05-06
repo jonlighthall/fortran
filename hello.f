9c1893474623dcf1e3aceac2b94515927afdb87e
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-06 12:54:32 -0500

    add iteration

--- hello.f
+++ hello.f
@@ -1,8 +1,8 @@
       program hello
-      print *, "hello world"
-      print *, "fortan"
-      do 10 i=0,2,1 !print beeps at end of program
-         print *, " beep!", char(7)
+      write(*,'(a)') "hello world"
+      write(*,'(a)') "fortan"
+      do 10 i=1,3,1             !print beeps at end of program
+         write(*,'(i2,3(a))')i+1,char(9),"beeps", char(7)
          call sleep(1)
  10   continue
       end 
