d218721f69a78126978d8c728ce59774f07ec199
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-06 10:00:59 -0500

    add marginal time

--- collatz_loop.f
+++ collatz_loop.f
@@ -1,6 +1,7 @@
       program collatz_loop
       implicit none
-      integer*16 i,j,k,n,nmx,dly,dlymx,dlyrec,sd
+      integer*4 dly,dlymx,dlyrec,t1,t2,t
+      integer*16 i,j,k,n,nmx,sd
       logical error
       i=1 
       j=0
@@ -17,16 +18,17 @@
          enddo
       enddo
       error=.false.
-      open(1,file = 'collatz.out',status='unknown',action='write')
+      open(1,file = 'collatz.out')!,status='unknown',action='write')
       write(*,*) '          the largest integer is', j
       write(1,*) '          the largest integer is', j
       nmx=(j-1)/3
       write(*,*) 'the largest hailstone integer is', nmx
       write(1,*) 'the largest hailstone integer is', nmx
-      write(*,*) 'record delay seed'
-      write(1,*) 'record delay seed'
+      write(*,*) 'record delay seed time'
+      write(1,*) 'record delay seed time'
       dlymx=0                   ! delay max
       dlyrec=0                  ! delay record
+      call system_clock(t1)
       do sd=0,nmx               ! seed range
          n=sd
          dly=0
@@ -46,10 +48,13 @@
             dly=dly+1         
          enddo
          if (dly>dlymx) then
-            dlyrec=dlyrec+1  
-            write(*,*)dlyrec,dly,sd
+            dlyrec=dlyrec+1
+            call system_clock(t2)
+            t=t2-t1
+            write(*,*)dlyrec,dly,sd,t
             write(1,*)dlyrec,dly,sd
             dlymx=dly
+            call system_clock(t1)
          endif
          if (error) exit
       enddo
