fb92f7a3b8897f46ef74404fb8c2b420d80741cc
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-10 12:15:40 -0500

    condensed, added exit loop write

--- interrupt.f
+++ interrupt.f
@@ -1,18 +1,15 @@
       subroutine catch_signal
-      logical :: interrupted
+      logical interrupted
       common interrupted
       interrupted = .true.
       write(*,*) 'in catch, interrupted = ',interrupted
-      end subroutine catch_signal
+      end 
 
       program crcount
-      integer :: i,the_start,the_end
-      integer :: stat
-      
-      logical :: interrupted
+      integer i,the_start,the_end,stat
+      logical interrupted
       common interrupted
       external catch_signal
-      
       integer, parameter :: SIGINT = 2
       
       call signal(SIGINT, catch_signal)
@@ -41,10 +38,11 @@
          write(*,'(i2)') i
          call Sleep(1)
       end do
+      if (i.ge.the_end) write(*,*)'end reached',the_end
+      write(*,*)'exit loop'
 
 !     Save current state
       open(1,file='state',status='REPLACE',action='WRITE',iostat=stat)
       write(1,'(i2)')i
       close(1)
-      
-      end program crcount
+      end 
