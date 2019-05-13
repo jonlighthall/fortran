fa2b3e83f72f60ccf0e8b877df2cee65710a7c87
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-13 09:21:25 -0500

    update var names, add path record search

--- collatz.f
+++ collatz.f
@@ -1,28 +1,33 @@
       program collatz
       implicit none
-      integer*16 n,i,seq,nmx
-      common nmx
+      integer*16 n,i,seq,sysmx,mxn,sd
+      common sysmx
       call findmx
       write(*,*)'enter arbitrary positive integer'
       read *,n
       write(*,*)'received input ',n
+      sd=n
       i=0
+      mxn=0
       do while (n.gt.1)
          write(*,*)'i = ',i,'ai = ',n
          n=seq(n)
+         if (n.gt.mxn) mxn=n
          i=i+1
       enddo
       write(*,*)'i = ',i,'ai = ',n
+      write(*,*)'Mx(N) = ',mxn
+      write(*,*)'for N = ',sd
       end
 
       integer*16 function seq(n)
       implicit none
-      integer*16 n,nmx
-      common nmx
+      integer*16 n,sysmx
+      common sysmx
       if (mod(n,2).eq.0) then
          seq=n/2
       else
-         if(n.gt.nmx) then
+         if(n.gt.sysmx) then
             write(*,*)'ERROR: Overflow imminent.'
             write(*,*)'Maximum hailstone size exceeded',n,3*n+1
             seq=1
@@ -35,8 +40,8 @@
 
       subroutine findmx
       implicit none
-      integer*16 i,j,k,nmx
-      common nmx
+      integer*16 i,j,k,sysmx
+      common sysmx
 c     find maximum integer
       i=1 
       j=0
@@ -53,7 +58,7 @@ c     find maximum integer
          enddo
       enddo
       write(*,*) '          the largest integer is', j
-      nmx=(j-1)/3
-      write(*,*) 'the largest hailstone integer is', nmx
+      sysmx=(j-1)/3
+      write(*,*) 'the largest hailstone integer is', sysmx
       return
       end
