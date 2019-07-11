dd51a49d0dff67481074e56453e6fed479e6689e
Jon Lighthall <jon.lighthall@gmail.com>
2019-07-11 11:25:22 -0500

    change prog name, add smallest int

--- extrema.f
+++ extrema.f
@@ -1,4 +1,4 @@
-      program new
+      program extrema
       print *, huge(0), huge(0.0), huge(0.0d0)
-      print *, tiny(0.0), tiny(0.0d0)
-      end program new
+      print *, 1, tiny(0.0), tiny(0.0d0)
+      end program extrema
