c80f2351b7cabd2ef9fe6446dc10f22d8892d4e3
Jon Lighthall <jon.lighthall@gmail.com>
2019-07-15 14:57:52 -0500

    change to in-line, add writes, closes

--- newunit_test.f
+++ newunit_test.f
@@ -2,10 +2,22 @@
       integer lun
       integer unit1,unit2,unit3
       open(unit=newunit(lun),file='test')
-      unit1=newunit(lun)
-      open(unit1,file='test1')
-      unit2=newunit(lun)
-      open(unit2,file='test2')
-      unit3=newunit(lun)
-      open(unit3,file='test3')
+      write(*,*) 'lun = ', lun
+c     unit1=newunit(lun)
+c     open(unit1,file='test1')
+      open(unit=newunit(unit1),file='test1')
+      write(*,*) 'unit1 = ', unit1
+c     unit2=newunit(lun)
+c     open(unit2,file='test2')
+      open(unit=newunit(unit2),file='test2')
+      write(*,*) 'unit2 = ', unit2
+c     unit3=newunit(lun)
+c     open(unit3,file='test3')
+      open(unit=newunit(unit3),file='test3')
+      write(*,*) 'unit3 = ', unit3
+      close(lun)
+      close(unit1)
+      close(unit2)
+      close(unit3)
+
       end program test_program
