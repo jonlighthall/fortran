eeb729a021064779593b32ac82581ee754e5d297
Jon Lighthall <jon.lighthall@gmail.com>
2019-07-15 14:26:51 -0500

    change test to use function

--- newunit_test.f
+++ newunit_test.f
@@ -1,4 +1,11 @@
       program test_program
-      integer :: unit
-      open(newunit=unit,file='test')
+      integer lun
+      integer unit1,unit2,unit3
+      open(unit=newunit(lun),file='test')
+      unit1=newunit(lun)
+      open(unit1,file='test1')
+      unit2=newunit(lun)
+      open(unit2,file='test2')
+      unit3=newunit(lun)
+      open(unit3,file='test3')
       end program test_program
