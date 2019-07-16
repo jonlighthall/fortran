cf3be237362d1c8867843b7dfaedd736ee14f3da
Jon Lighthall <jon.lighthall@gmail.com>
2019-07-16 16:06:30 -0500

    add both separate and combined times

--- test_system_clock.f
+++ test_system_clock.f
@@ -1,13 +1,24 @@
       PROGRAM test_system_clock
       implicit none
-      INTEGER :: count, count_rate, count_max,remain,sec,min,hr,day
+      INTEGER :: count, count_rate, count_max,remain,sec,min,hr
+     &     ,day
       integer delay,elap
       character(32) fmt
       CALL SYSTEM_CLOCK(count, count_rate, count_max)
       WRITE(*,*) count, count_rate, count_max
       remain=count_max-count
-      WRITE(*,*) 'remain = ',remain
-      write(fmt,*)'(a,t11,i2,a)'
+      write(*,*) 'There are ',remain,' units'
+      sec=remain/count_rate
+      write(*,*) '       or ',sec,' seconds'
+      min=remain/count_rate/60
+      write(*,*) '       or ',min,' minutes'
+      hr=remain/count_rate/60/60
+      write(*,*) '       or ',hr,' hours'
+      day=remain/count_rate/60/60/24
+      write(*,*) '       or ',day,
+     &' days remaining before system clock rollover'
+      write(*,*)repeat('-',30)
+      write(fmt,*)'(1x,a,t12,i2,a)'    
       day=remain/count_rate/60/60/24
       write(*,fmt) 'There are ',day,' days'
       hr=(remain/count_rate-day*60*60*24)/60/60
@@ -19,6 +30,7 @@
      &     ,' seconds remaining before system clock rollover'
       call sleep(10)
       CALL SYSTEM_CLOCK(delay)
+      delay=15
       if(delay.le.count) then
          write(*,*)'rollover suspected'
          elap=(delay+count_max-count)/count_rate
