fdfe2518db051269f7aad0ea54fabab1662ad5a4
Jon Lighthall <jon.lighthall@gmail.com>
2019-07-16 15:11:38 -0500

    add time before rollover

--- test_system_clock.f
+++ test_system_clock.f
@@ -1,5 +1,9 @@
       PROGRAM test_system_clock
-      INTEGER :: count, count_rate, count_max
+      implicit none
+      INTEGER :: count, count_rate, count_max,remain
       CALL SYSTEM_CLOCK(count, count_rate, count_max)
       WRITE(*,*) count, count_rate, count_max
+      remain=count_max-count
+      write(*,*) 'There are ',remain/count_rate,' seconds remaining
+     &     before rollover'
       END PROGRAM
