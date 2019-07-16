6efdde8bdaacefed9368a72ea8732d05d599c4e9
Jon Lighthall <jon.lighthall@gmail.com>
2019-07-16 15:15:37 -0500

    add hours,min, days

--- test_system_clock.f
+++ test_system_clock.f
@@ -1,9 +1,21 @@
       PROGRAM test_system_clock
       implicit none
-      INTEGER :: count, count_rate, count_max,remain
+      INTEGER :: count, count_rate, count_max,remain,sec,min,hr,day
       CALL SYSTEM_CLOCK(count, count_rate, count_max)
       WRITE(*,*) count, count_rate, count_max
       remain=count_max-count
-      write(*,*) 'There are ',remain/count_rate,' seconds remaining
+      sec=remain/count_rate
+      write(*,*) 'There are ',sec,' seconds remaining
      &     before rollover'
+      min=remain/count_rate/60
+      write(*,*) 'There are ',min,' minutes remaining
+     &     before rollover'
+      hr=remain/count_rate/60/60
+      write(*,*) 'There are ',hr,' hours remaining
+     &     before rollover'
+      day=remain/count_rate/60/60/24
+      write(*,*) 'There are ',day,' days remaining
+     &     before rollover'
+
+
       END PROGRAM
