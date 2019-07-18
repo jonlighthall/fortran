b24216bc6284a738f56d9b79c229b2da3e9795f0
Jon Lighthall <jon.lighthall@gmail.com>
2019-07-18 16:25:47 -0500

    clean up prints, add min and max run times

--- test_system_clock.f
+++ test_system_clock.f
@@ -7,22 +7,19 @@
       WRITE(*,*) count, count_rate, count_max
       remain=count_max-count
       call format(remain,str)
-      write(*,*) 'There are ',remain,' units'
       write(*,*) 'There are ',trim(adjustl(str)),' units'
       sec=remain/count_rate
       call format(sec,str)
-      write(*,*) '       or ',sec,' seconds'
       write(*,*) '       or ',trim(adjustl(str)),' seconds'
       min=remain/count_rate/60
       call format(min,str)
       write(*,*) '       or ',trim(adjustl(str)),' minutes'
-      write(*,*) '       or ',min,' minutes'
       hr=remain/count_rate/60/60
       call format(hr,str)
-      write(*,*) '       or ',hr,' hours'
       write(*,*) '       or ',trim(adjustl(str)),' hours'
       day=remain/count_rate/60/60/24
-      write(*,*) '       or ',day,
+      call format(day,str)
+      write(*,*) '       or ',trim(adjustl(str)),
      &' days remaining before system clock rollover'
       write(*,*)repeat('-',30)
       write(fmt,*)'(1x,a,t12,i2,a)'    
@@ -35,10 +32,16 @@
       sec=(remain/count_rate-day*60*60*24-hr*60*60-min*60)
       write(*,fmt) '      and ',sec
      &     ,' seconds remaining before system clock rollover'
-c      call sleep(10)
-      CALL SYSTEM_CLOCK(delay)
-c      delay=15
-      if(delay.le.count) then
+c     run for one cycle
+      delay=count
+      do while (delay.le.count)
+         call system_clock(delay)
+      enddo
+c     run until rollover
+      call sleep(remain/count_rate+10)
+
+      WRITE(*,*) delay, count_rate, count_max
+      if(delay.lt.count) then
          write(*,*)'rollover suspected'
          elap=(delay+count_max-count)/count_rate
       else
