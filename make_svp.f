4da3e4cf7778e70cb9427f8693d3abf1f5eb6ce3
Jon Lighthall <jon.lighthall@gmail.com>
2019-07-19 12:44:03 -0500

    edit output to match real infile

--- make_svp.f
+++ make_svp.f
@@ -9,11 +9,13 @@
          write(*,*)(i-1)*250,rand(),rand(),rand()
          write(1,*)(i-1)*250
          do j=1,10
+c     NOTE: screen print and file contents will not match
             write(*,*) j,(j-1)*2.5,(rand()-0.5)*20+1500,(rand()-0.5)*20
             write(1,*) j,(j-1)*2.5,(rand()-0.5)*20+1500,(rand()-0.5)*20
          enddo
          write(*,*)repeat(' -1 ',4)
-         write(1,*)repeat(' -1 ',4)
+         write(1,*)repeat(' -1 ',2)
       enddo
+      write(1,*)' -1'
       close(1)
       end
