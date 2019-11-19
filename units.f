cd94c915b6777e7c28f956e9dbb5f3e4d7e4ad42
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-19 15:45:02 -0600

    add db_yd2m

--- units.f
+++ units.f
@@ -1,6 +1,7 @@
       program units
       integer(kind=2), parameter :: nmi2m=1852, ift2m=3048
-      real(8) ft2m,m2ft,m2nmi,m2yd,dB_m2yd
+c      double precision, parameter :: ft2m=0.3048
+      real(8) ft2m,m2ft,m2nmi,m2yd,dB_m2yd,dB_yd2m
       write(*,*)'nmi2m = ',nmi2m
       ft2m=ift2m/1e4
       write(*,*)'ft2m = ',ft2m
@@ -10,6 +11,11 @@
       write(*,*)'m2nmi = ',m2nmi
       m2yd=m2ft/3
       write(*,*)'m2yd = ',m2yd
+      yd2m=1/m2yd
+      write(*,*)'yd2m = ',yd2m
       dB_m2yd=20*log10(m2yd)
       write(*,*)'dB_m2yd = ',dB_m2yd
+      dB_yd2m=20*log10(yd2m)
+      write(*,*)'dB_yd2m = ',dB_yd2m
+
       end
