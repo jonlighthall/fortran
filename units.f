53ca11478a14c5d02ce8f28d56393f892c445895
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-19 15:37:33 -0600

    add db_m2yd

--- units.f
+++ units.f
@@ -1,6 +1,6 @@
       program units
-      integer, parameter :: nmi2m=1852, ift2m=3048
-      real ft2m,m2ft,m2nmi,db_m2yd
+      integer(kind=2), parameter :: nmi2m=1852, ift2m=3048
+      real(8) ft2m,m2ft,m2nmi,m2yd,dB_m2yd
       write(*,*)'nmi2m = ',nmi2m
       ft2m=ift2m/1e4
       write(*,*)'ft2m = ',ft2m
@@ -8,4 +8,8 @@
       write(*,*)'m2ft = ',m2ft
       m2nmi=1./nmi2m
       write(*,*)'m2nmi = ',m2nmi
+      m2yd=m2ft/3
+      write(*,*)'m2yd = ',m2yd
+      dB_m2yd=20*log10(m2yd)
+      write(*,*)'dB_m2yd = ',dB_m2yd
       end
