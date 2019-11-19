31f663655b214cb05d31843d6fd62ef88caa5fa7
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-19 16:31:04 -0600

    add selected real kind

--- units.f
+++ units.f
@@ -1,8 +1,10 @@
       program units
       implicit none
       integer, parameter :: nmi2m=1852, ift2m=3048
-      real(kind = 10) ft2m,m2ft,m2nmi,m2yd,yd2m,dB_m2yd,dB_yd2m
-      write(*,*)'nmi2m = ',nmi2m
+      integer, parameter :: dp = 8
+      real(kind = selected_real_kind(dp)) ft2m,m2ft,m2nmi,m2yd,yd2m
+     &     ,dB_m2yd,dB_yd2m
+      write(*,*)'  nmi2m = ',nmi2m
       ft2m=ift2m/1e4
       write(*,*)'ft2m = ',ft2m
       m2ft=1/ft2m
