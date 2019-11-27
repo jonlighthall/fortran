02790917dccd366319716c7b70cfb10b67e2b5a1
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-27 13:09:07 -0600

    remove tab

--- hello.f
+++ hello.f
@@ -2,7 +2,7 @@
       write(*,'(a)') "hello world"
       write(*,'(a)') "fortan"
       do 10 i=1,3,1             !print beeps at end of program
-         write(*,'(i2,3(a))')i+1,char(9),"beeps", char(7)
+         write(*,'(i2,2(a))')i," beeps", char(7)
          call sleep(1)
  10   continue
       end 
