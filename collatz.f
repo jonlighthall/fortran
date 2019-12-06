3c1f98f8c36e7b6e544af0ab3dcd736b7a884136
Jon Lighthall <jon.lighthall@gmail.com>
2019-12-06 17:27:20 -0600

    remove contained function seq

--- collatz.f
+++ collatz.f
@@ -1,7 +1,7 @@
       program collatz
       implicit none
       include 'set_format.f'
-      integer(kind=intsize) n,i,sysmx,mxn,sd
+      integer(kind=intsize) n,i,sysmx,mxn,sd,seq
       common sysmx
       sysmx=huge(i)
       write(*,*) '          the largest integer is', sysmx
@@ -15,30 +15,22 @@
       mxn=0
       do while (n.gt.1)
          write(*,*)'i = ',i,'ai = ',n
-         n=seq(n)
+         if (mod(n,2).eq.0) then
+            seq=n/2
+         else
+            if(n.gt.sysmx) then
+               write(*,*)'ERROR: Overflow imminent.'
+               write(*,*)'Maximum hailstone size exceeded',n,3*n+1
+               seq=1
+            else
+               seq=3*n+1
+            endif
+         endif
+         n=seq
          if (n.gt.mxn) mxn=n
          i=i+1
       enddo
       write(*,*)'i = ',i,'ai = ',n
       write(*,*)'Mx(N) = ',mxn
       write(*,*)'for N = ',sd
-      
-      contains
-      integer(kind=intsize)function seq(n)
-      implicit none
-      integer(kind=intsize) n,sysmx
-      common sysmx
-      if (mod(n,2).eq.0) then
-         seq=n/2
-      else
-         if(n.gt.sysmx) then
-            write(*,*)'ERROR: Overflow imminent.'
-            write(*,*)'Maximum hailstone size exceeded',n,3*n+1
-            seq=1
-         else
-            seq=3*n+1
-         endif
-      endif
-      return
-      end function
       end program
