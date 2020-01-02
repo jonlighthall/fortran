90512adcb8f064dbfc5ea69d33a8dcaeb8cb6327
Jon Lighthall <jon.lighthall@gmail.com>
2020-01-02 12:41:45 -0600

    add self-limiting loop

--- basel.f
+++ basel.f
@@ -1,17 +1,22 @@
       program units
       implicit none
-      integer, parameter :: dp = 15
-      integer, parameter :: srk = selected_real_kind(dp)
-      real(kind = srk), parameter :: pi = 4d0*atan(1d0)
-      integer i
-      real(kind = srk) sum,pival
+      real(kind = 10), parameter :: pi = 4q0*atan(1q0)
+      integer (kind=16) i
+      real(kind = 10) sum,pival,seq
+      character (len=128) pistr
+      pistr = '  3.1415926535897932384626433832795028841971693993751058'
+      write(*,*)trim(pistr)
       write(*,*)pi
       sum=0
-      do i=1,50000
-         sum=sum + 1d0/(i*i)
-         pival=sqrt(6d0*sum)
-
-
+      i=1
+      pival=0
+      seq=2
+      do while (seq.gt.1q0/(i*i))
+         seq=1q0/(i*i)
+         sum=sum + seq
+         pival=sqrt(6q0*sum)
+         i=i+1
       enddo
+      write(*,*)pival
       write(*,*)i,sum,pival
       end
