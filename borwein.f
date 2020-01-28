672b5c5aa0bdeaab0b3675b6097a1bad0d9a7050
Jon Lighthall <jon.lighthall@gmail.com>
2020-01-28 11:23:19 -0600

    unify pi tests

--- borwein.f
+++ borwein.f
@@ -1,31 +1,35 @@
       program borwein
-      real(16)a,b,p,sum
-      integer(16) ii,i,j,l
+      implicit none
+      real(16)a,b,p
+      integer(16) ii,i,ipi,test_val
       character (len=40) ipistr
-      logical  valok
+      logical  val_ok
       a=sqrt(2d0)
       b=0d0
       p=2d0+sqrt(2d0)
-       ipistr = '31415926535897932384626433832795028841971693993751058'
-      do i=1,5
+      ipistr = '31415926535897932384626433832795028841971693993751058'
+      i=0
+      do while (a.ne.1d0)
          b=((1d0+b)*sqrt(a))/(a+b)
          a=(sqrt(a)+1d0/sqrt(a))/2d0
          p=((1d0+a)*p*b)/(1d0+b)
          write(*,*)i,a,b,p
 
+c     compare digits
          ii=0
-         valok=.true.
-         do while (valok)
-            sum=p*10**ii
-            j=floor(sum,16)
-            read(ipistr(1:ii+1),*)l
-            write(*,*)ii+1,j,l,j.eq.l
-            if ((j.ne.l)) then
-               valok=.false.
+         val_ok=.true.
+         do while (val_ok)
+            ipi=floor(p*10**ii,16)
+            read(ipistr(1:ii+1),*)test_val
+            write(*,*)ii+1,ipi,test_val,ipi.eq.test_val
+            if ((ipi.ne.test_val)) then
+               val_ok=.false.
+               write(*,'(1x,i2,a)')ii
+     $              ,' decimal places of precision achieved'
                exit
             endif
             ii=ii+1
          enddo
-         
+         i=i+1
       enddo
       end 
--- test_pi.f
+++ test_pi.f
@@ -1,34 +1,32 @@
       program test_pi
       implicit none
-      integer (kind=16) i,pdp,j,k,l
+      integer (kind=16) i,ii,ipi,test_val,pdp
       integer, parameter :: dp = 15
       integer, parameter :: srk = selected_real_kind(dp)
-      real(kind = srk), parameter :: pi = 4.d0*atan(1.d0)
-      real(kind = srk) pireal,sum
-      character (len=40) pistr,fmt,ipistr
+      real(kind = srk), parameter :: p = 4.d0*atan(1.d0)
+      character (len=40) ipistr,fmt
+      logical  val_ok
       write(*,'(1x,i2,a,i2,a)')dp,' decimals reqested (using ',srk
      &     ,' bytes)'
-      pdp=dp-1
-      write(fmt,'(a,i0.2,a,i0.2,a)')'(3x,f',pdp+2,'.',pdp,',a)'
-      pistr = '3.1415926535897932384626433832795028841971693993751058'
       ipistr = '31415926535897932384626433832795028841971693993751058'
-      write(*,'(3x,2a)')trim(pistr(1:dp+2)),' string'
-      read(pistr,*)pireal
-      write(*,fmt)pireal,'  real'
-      write(*,fmt)pi,'  atan'
+      write(*,'(3x,4a)')ipistr(1:1),'.',ipistr(2:dp+1),' string'
+      pdp=dp-1                  ! printed decimal places
+      write(fmt,'(a,i0.2,a,i0.2,a)')'(3x,f',pdp+2,'.',pdp,',a)'
+      write(*,fmt)p,'  atan'
 
 c     compare digits
-      pdp=len(trim(ipistr))
-      write(fmt,'(a,i0.2,a,i0.2,a,i0.2,a)')'(1x,i2,i',pdp,',i',pdp,',i'
-     &     ,pdp,',1x,l1,l1)'
-      do i=0,pdp-2
-         sum=pireal*10**i
-         j=floor(sum,16)
-         sum=pi*10**i
-         k=floor(sum,16)
-         read(ipistr(1:i+1),*)l
-         write(*,fmt)i+1,j,k,l,j.eq.k,k.eq.l
-         if ((j.ne.k).or.(k.ne.l)) exit
+      ii=0
+      val_ok=.true.
+      do while (val_ok)
+         ipi=floor(p*10**ii,16)
+         read(ipistr(1:ii+1),*)test_val
+         write(*,*)ii+1,ipi,test_val,ipi.eq.test_val
+         if ((ipi.ne.test_val)) then
+            val_ok=.false.
+            write(*,'(1x,i2,a)')ii
+     $           ,' decimal places of precision achieved'
+            exit
+         endif
+         ii=ii+1
       enddo
-      write(*,'(1x,i2,a)')i,' decimal places of precision achieved'
       end
