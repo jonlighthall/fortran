494abed1db8ddf9f40cdd12a51f6bb6785db508c
Jon Lighthall <jon.lighthall@gmail.com>
2020-02-04 16:05:56 -0600

    add labels

--- extrema.f
+++ extrema.f
@@ -1,4 +1,6 @@
       program extrema
+      write(*,'(t3,a,t16,a,t33,a,t59,a)')'integer','real','double'
+     $     ,'quad'
       print *, huge(0), huge(0e0), huge(0d0), huge(0q0)
       print *, 1, tiny(0e0), tiny(0d0), tiny(0q0)
       end program extrema
