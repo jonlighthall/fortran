46b6539c2171c179617b0e9734245c97368fb955
Jon Lighthall <jon.lighthall@gmail.com>
2020-02-04 16:01:54 -0600

    add double, quad

--- extrema.f
+++ extrema.f
@@ -1,4 +1,4 @@
       program extrema
-      print *, huge(0), huge(0.0), huge(0.0d0)
-      print *, 1, tiny(0.0), tiny(0.0d0)
+      print *, huge(0), huge(0e0), huge(0d0), huge(0q0)
+      print *, 1, tiny(0e0), tiny(0d0), tiny(0q0)
       end program extrema
