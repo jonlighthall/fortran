c81fa8e69618fad84acb078668be5815024cd701
Jon Lighthall <jon.lighthall@gmail.com>
2021-02-09 09:10:37 -0600

    fix seeding

--- dice.f
+++ dice.f
@@ -4,13 +4,40 @@ c     old
       integer i,j,k,count,r2d,rolls
       real u(2)
       logical jail
-      call system_clock(count)  ! get system time in milliseconds
-      call random_seed(count)
-c     
-c     new
+
+      integer :: seed_mode=1
+      
+      integer, allocatable :: seed(:)
+      integer :: n
+      call random_seed(size = n)
+      allocate(seed(n))
+
+
+      select case (seed_mode)
+
+      case (1)
+         seed(:)=1
+         call random_seed(put=seed)
+
+      case (2)
+         call system_clock(count) ! get system time in milliseconds
+         write(*,*)count
+         seed(:)=count
+c     seed(:)=seed+count
+         write(*,*)seed
+         call random_seed(put=seed)
+         call random_seed(get=seed)
+         write (*, *) seed
+
+      case default
+         call random_seed()
+         call random_seed(get=seed)
+         write (*, *) seed
+      end select
+      
       jail=.false.
       rolls=0;
-c      do while (.not. jail)
+c     do while (.not. jail)
       do i=1,10
          call random_number(u)
          j = r2d(u(1))
@@ -36,7 +63,7 @@ c      do while (.not. jail)
       enddo
       write(*,*)"it took ",rolls,"to go to jail"
       end
-c
+c     
       integer function r2d(u)
 c     random to dice
       real u
