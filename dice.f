705d8d26b7e44745a1c01885ca18213c7ccf1a8e
Jon Lighthall <jon.lighthall@gmail.com>
2021-02-22 09:24:00 -0600

    add fmt write, add count, clean up seed

--- dice.f
+++ dice.f
@@ -1,67 +1,80 @@
       program dice
       implicit none
-c     old
-      integer i,j,k,count,r2d,rolls
+      integer i,j,k,time,r2d,rolls
       real u(2)
       logical jail
-
       integer :: seed_mode=1
-      
       integer, allocatable :: seed(:)
       integer :: n
+      integer :: count(6)
+
       call random_seed(size = n)
       allocate(seed(n))
-
-
       select case (seed_mode)
-
       case (1)
          seed(:)=1
          call random_seed(put=seed)
-
       case (2)
-         call system_clock(count) ! get system time in milliseconds
-         write(*,*)count
-         seed(:)=count
-c     seed(:)=seed+count
-         write(*,*)seed
+         call system_clock(count=time) ! get system time in milliseconds
+         call random_seed(get=seed)
+         write(*,*)'seed is'
+         write(*,*) seed
+         write(*,*)'The time is ',time
+         seed(:)=time
          call random_seed(put=seed)
          call random_seed(get=seed)
-         write (*, *) seed
-
+         write(*,*)'seed is'
+         write (*,*) seed
       case default
          call random_seed()
-         call random_seed(get=seed)
-         write (*, *) seed
+c     call random_seed(get=seed)
+c     write (*, *) seed
       end select
       
       jail=.false.
-      rolls=0;
-c     do while (.not. jail)
-      do i=1,10
+      count=0
+      rolls=0
+      i=1
+      do while (.not. jail)
+         
+c      do i=1,5
          call random_number(u)
          j = r2d(u(1))
          k = r2d(u(2))
-         write(*,*)j,k
+         write(*,'(a,i3,a,i1,a,i1,a,i2)')'Roll ',i,':',j,'+',k,'=',j+k
+         count(j)=count(j)+1
+         count(k)=count(k)+1
          if (j.eq.k) then       ! first double
             call random_number(u)
             j = r2d(u(1))
             k = r2d(u(2))
-            write(*,*)j,k,"second roll"
+            count(j)=count(j)+1
+            count(k)=count(k)+1
+            write(*,'(a,i1,a,i1,a,i2,a)')'        :',j,'+',k,'=',j
+     &           +k,' second roll'
             if (j.eq.k) then    ! second double
                call random_number(u)
                j = r2d(u(1))
                k = r2d(u(2))
-               write(*,*)j,k, "third roll"
+               count(j)=count(j)+1
+               count(k)=count(k)+1
+               write(*,'(a,i1,a,i1,a,i2,a)')'        :',j,'+',k,'=',j+k
+     &              ,' third roll'
+
                if (j.eq.k) then
-                  write(*,*)"JAIL"
+                  write(*,*)'JAIL'
                   jail=.true.
                endif
             endif
          endif
          rolls=rolls+1
+         i=i+1
+
       enddo
-      write(*,*)"it took ",rolls,"to go to jail"
+      if(jail)then
+         write(*,*)'it took ',rolls,'to go to jail'
+      endif
+      write(*,*)count
       end
 c     
       integer function r2d(u)
