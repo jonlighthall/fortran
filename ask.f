06dc980a79174242cc29d4383fb566f9fd35c6a7
Jon Lighthall <jon.lighthall@gmail.com>
2021-04-16 13:07:12 -0500

    add test function

--- ask.f
+++ ask.f
@@ -1,17 +1,30 @@
       program ask
-      logical ans
+      logical ans,yea
       character words
       ans=.false.
       write(*,*)'give me an answer (y/n)'
       read(5,*)words
       write(*,*)words
-
       
       if(index('yY',words).gt.0) then
          ans=.true.
       else
          ans=.false.
       endif
-
       write(*,*)ans
+
+c     check response with function
+      ans=yea(words)
+      write(*,*)ans
+      end
+
+c     check if response is positive
+      logical function yea(text)
+      character text
+      if(index('yY',text).gt.0) then
+         yea=.true.
+      else
+         yea=.false.
+      endif
+      return
       end
