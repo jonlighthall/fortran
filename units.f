8eb4035a5c1c11f06d76669f1f7d8e48b1094dc3
Jon Lighthall <jon.lighthall@gmail.com>
2022-04-06 12:30:38 -0500

    add comp real

--- units.f
+++ units.f
@@ -1,17 +1,12 @@
-      program units
+ 1    program units
       implicit none
 
       include "metrics_revised2.inc"
-c      integer, parameter :: set_precision = 19
-c      integer, parameter :: set_kind = selected_real_kind(set_precision)
-      integer dummy,comp_real_str
+      integer dummy,comp_real_str,comp_real
       real(kind = set_kind), parameter :: dB_yd2m=20d0*log10(m2ft/3q0)
       character(len = 256) :: fmt3,fmt2
-      integer(kind=16) ii,i1,i2,ipi,i_test,i_ref,i,i_dum
-      real(kind=16)real_value,real_val
-      logical  val_ok
       character (len=40) str_m2ft,str_m2mi,str_dB_m2yd,str_ms2kt
-     &     ,str_kt2ms,test_str,str_ref
+     &     ,str_kt2ms
 
 c     equivalence definitions
       integer, parameter :: ift2m=3048
@@ -28,7 +23,7 @@ c     10    18  14
 c     16    33  32
       integer, parameter :: dp = 33
       integer, parameter :: srk = selected_real_kind(dp)
-      real(kind = srk) nmi2m,qft2m,qm2ft,m2nmi,m2yd,yd2m,qdB_m2yd
+      real(kind = srk) qnmi2m,qft2m,qm2ft,m2nmi,m2yd,yd2m,qdB_m2yd
      &     ,qdB_yd2m,qkt2ms,qms2kt,qans
       character(len = 256) :: fmt
       real(kind = 4) snmi2m,sm2nmi,sft2m,sm2ft,sm2yd,syd2m,sdB_m2yd
@@ -57,7 +52,8 @@ c     ----------------
       write(*,*) ' prec dB_m2yd = ',precision(dB_m2yd)
       write(*,*) ' prec kt2ms   = ',precision(kt2ms)
       write(*,*) ' prec ms2kt   = ',precision(ms2kt)
-      
+
+      dummy=comp_real(abs(dB_m2yd),abs(dB_yd2m))
       
 c     ----------------
 c     SINGLE PRECISION
@@ -92,6 +88,11 @@ c     printed output
       write(*,*  )' m2nmi = ',sm2nmi
       write(*,fmt)'  kt2ms = ',skt2ms
       write(*,fmt)'  ms2kt = ',sms2kt
+
+      write(*,*) 'kind ', kind(sdB_m2yd)
+      write(*,*) 'precision ',precision(sdB_m2yd)
+
+      dummy=comp_real(real(abs(sdB_m2yd),16),real(abs(sdB_yd2m),16))     
       
 c     check differnece
       write(*,*)
@@ -121,24 +122,13 @@ c     check differnece
       sans = sms2kt - ms2kt
       write(*,fmt)'   ms2kt - ms2kt   = ',  sans
 
-c     compare digits
-      write(*,*)'comparing digits...'
-      ii=0
-      val_ok=.true.
-      do while (val_ok.and.(ii.lt.(pdp+2)))
-         i1=floor(-dB_m2yd*10**ii,16)
-         i2=floor(sdB_m2yd*10**ii,16)
-         write(*,*)ii+1,i1,i2,i1.eq.i2
-         if ((i1.ne.(1*i2))) then
-            val_ok=.false.
-            write(*,'(1x,i2,a)')ii
-     $           ,' decimal places of precision achieved'
-            exit
-         endif
-         ii=ii+1
-      enddo
+c      dummy=comp_real(m2ft,sm2ft)
+c      dummy=comp_real(m2mi,sm2nmi)
+c      dummy=comp_real(abs(dB_m2yd),abs(sdB_m2yd))
+c      dummy=comp_real(abs(dB_yd2m),abs(sdB_yd2m))
+c      dummy=comp_real(ms2kt,sms2kt)
+c      dummy=comp_real(kt2ms,skt2ms) 
 
-      
       
 c     ----------------
 c     DOUBLE PRECISION
@@ -190,24 +180,15 @@ c     printed output
       dans = dms2kt - ms2kt
       write(*,fmt)'   ms2kt - ms2kt   = ',  dans
 
-c     compare digits
-      write(*,*)'comparing digits...'
-      ii=0
-      val_ok=.true.
-      do while (val_ok.and.(ii.lt.(pdp+2)))
-         i1=floor(-dB_m2yd*10**ii,16)
-         i2=floor(ddB_m2yd*10**ii,16)
-         write(*,*)ii+1,i1,i2,i1.eq.i2
-         if ((i1.ne.(1*i2))) then
-            val_ok=.false.
-            write(*,'(1x,i2,a)')ii
-     $           ,' decimal places of precision achieved'
-            exit
-         endif
-         ii=ii+1
-      enddo
+c      dummy=comp_real(m2ft,dm2ft)
+c      dummy=comp_real(m2mi,dm2nmi)
+c      dummy=comp_real(abs(dB_m2yd),abs(ddB_m2yd))
+c      dummy=comp_real(abs(dB_yd2m),abs(ddB_yd2m))
+c      dummy=comp_real(ms2kt,dms2kt)
+c      dummy=comp_real(kt2ms,dkt2ms) 
 
-      
+c      dummy=comp_real(abs(ddB_m2yd),abs(ddB_yd2m))
+
 c     --------------
 c     QUAD PRECISION
 c     --------------      
@@ -235,17 +216,17 @@ c     formatting
       write(*,fmt)'real bytes = ',srk
       
 c     equivalence definitions
-      nmi2m=1852q0
+      qnmi2m=1852q0
 
 c     functional definitions
-      m2nmi=1q0/nmi2m
+      m2nmi=1q0/qnmi2m
       qms2kt=m2nmi*6q1*6q1
       qkt2ms=1q0/qms2kt
       
 c     printed output
       write(fmt,'(a,i0.2,a,i0.2,a)')'(a,f',pdp+3,'.',pdp-3,')'
 c     write(fmt,'(a,i0.2,a,i0.2,a)')'(a,e',pdp+7,'.',pdp,')'
-      write(*,fmt)'  nmi2m = ',nmi2m
+      write(*,fmt)'  nmi2m = ',qnmi2m
 
       write(fmt,'(a,i0.2,a,i0.2,a)')'(a,f',pdp+3,'.',pdp,')'
 c     write(fmt,'(a,i0.2,a,i0.2,a)')'(a,e',pdp+7,'.',pdp,')'
@@ -271,23 +252,9 @@ c     printed output
       write(*,fmt)'dB_m2yd = ',qdB_m2yd
       write(*,fmt)'dB_yd2m = ',qdB_yd2m
 
-c     compare digits
-      write(*,*)'comparing digits...'
-      ii=0
-      val_ok=.true.
-      do while (val_ok.and.(ii.lt.(pdp+2)))
-         i1=floor(-dB_m2yd*10**ii,16)
-         i2=floor(qdB_m2yd*10**ii,16)
-         write(*,*)ii+1,i1,i2,i1.eq.i2
-         if ((i1.ne.(1*i2))) then
-            val_ok=.false.
-            write(*,'(1x,i2,a)')ii
-     $           ,' decimal places of precision achieved'
-            exit
-         endif
-         ii=ii+1
-      enddo
 
+      dummy=comp_real(abs(qdB_m2yd),abs(qdB_yd2m))
+      
       write(*,'(a,i2)')'   set precision is ',set_precision      
       pdp=precision(ft2m)
       write(*,'(a,i2)')'actual precision is ',pdp
@@ -307,24 +274,14 @@ c     compare digits
       write(*,fmt3)'dB_m2yd = ',dB_m2yd
       write(*,fmt3)'dB_yd2m = ',dB_yd2m
       write(*,fmt3)'  kt2ms = ',kt2ms
-      write(*,fmt3)'  ms2kt = ',ms2kt      
-      
-c     compare digits
-      write(*,*)'comparing digits...'
-      ii=0
-      val_ok=.true.
-      do while (val_ok.and.(ii.lt.(pdp+2)))
-         i1=floor(-dB_m2yd*10**ii,16)
-         i2=floor(dB_yd2m*10**ii,16)
-         write(*,*)ii+1,i1,i2,i1.eq.i2
-         if ((i1.ne.(1*i2))) then
-            val_ok=.false.
-            write(*,'(1x,i2,a)')ii
-     $           ,' decimal places of precision achieved'
-            exit
-         endif
-         ii=ii+1
-      enddo
+      write(*,fmt3)'  ms2kt = ',ms2kt  
+
+      dummy=comp_real(m2ft,qm2ft)
+      dummy=comp_real(m2mi,m2nmi)
+      dummy=comp_real(abs(dB_m2yd),abs(qdB_m2yd))
+      dummy=comp_real(abs(dB_yd2m),abs(qdB_yd2m))
+      dummy=comp_real(ms2kt,qms2kt)
+      dummy=comp_real(kt2ms,qkt2ms) 
 
 c     test functional definitions against strings
 
@@ -340,6 +297,8 @@ c                 1234567890123456789012345678901234567890
       dummy=comp_real_str(abs(dB_m2yd),str_dB_m2yd)
       dummy=comp_real_str(ms2kt,str_ms2kt)
       dummy=comp_real_str(kt2ms,str_kt2ms)
+
+      dummy=comp_real(real(100,16),real(100,16))
       
       end
 
@@ -347,20 +306,23 @@ c                 1234567890123456789012345678901234567890
       implicit none
       integer(kind=16) i,i_dum,i_ref,i_test,ii
       real(kind=16) real_val
-      character (len=40) str_ref
-      
+      character (len=40) str_ref      
       logical  val_ok
+
+      write(*,*)log10(real_val)
+      write(*,*)floor(log10(real_val))
+      write(*,*)real_val*10**(-1q0*floor(log10(real_val)))
 
 c     compare digits
       write(*,*)'comparing digits... (function)'
       ii=0
       val_ok=.true.
       write(*,'(a,f40.38)')'     test value is ',real_val
-      write(*,'(a,a)')'reference value is ',str_ref
+      write(*,'(a,a,a)')'reference value is ',str_ref,' (string)'
       i = index(str_ref,'.')
       write(*,*)'decimal point found at ',i
       
-      do while (val_ok)
+      do while (val_ok.and.(ii.lt.len(str_ref)))
          i_test=floor(real_val*10**ii,16)
          if (ii+1.lt.i) then
             read(str_ref(1:ii+1),*)i_ref
@@ -377,8 +339,82 @@ c     compare digits
      $           ,' decimal places of precision achieved'
             comp_real_str=ii
             return
-c            exit
          endif
          ii=ii+1
       enddo
+      end
+
+      integer function comp_real(real_val,real_ref)
+      implicit none
+c      integer,parameter :: ln1=precision(real_val)
+c      integer,parameter :: ln2=precision(real_ref)
+      integer(kind=16) i_ref,i_test,ii,a,pdp
+      real(kind=16) real_val,real_ref,b,sci_val,sci_ref
+      logical  val_ok
+      character(len=40) fmt
+
+c     convert to scientific notation
+      write(*,*)'convert digits...'
+      write(*,*)log10(real_val)
+      write(*,*)floor(log10(real_val))
+      write(*,*)real_val*10**(-1q0*floor(log10(real_val)))
+
+      a=floor(log10(real_val))
+      write(*,*)'a = ',a      
+      b=10**(-1q0*a)
+      write(*,*)'b = ',b
+
+      write(*,*)'test real_val = ',real_val*b
+      write(*,*)'test real_ref = ',real_ref*b
+      
+      sci_val=real_val*b
+      write(*,*)'here'
+      sci_ref=real_ref*b
+
+      write(*,*)'test real_val = ',sci_val
+      write(*,*)'test real_ref = ',sci_ref
+
+      pdp=precision(real_val)
+
+c     print digits
+      if(a.gt.0) then
+         write(fmt,'(a,i0.2,a,i0.2,a)')'(a,f',pdp+2,'.',pdp-a,')'
+      else
+         write(fmt,'(a,i0.2,a,i0.2,a)')'(a,f',pdp+2,'.',pdp,')'
+         endif
+      write(*,*)fmt
+      write(*,fmt)'     test value is ',real_val
+      write(*,fmt)'reference value is ',real_ref
+      write(*,*)'test precision = ',pdp
+      write(*,*)' ref precision = ',precision(real_ref)
+      write(*,*)'test kind = ',kind(real_val)
+      write(*,*)' ref kind = ',kind(real_ref)           
+      
+c     compare digits
+      write(*,*)'comparing digits... (function)'
+      ii=0
+      val_ok=.true.      
+      do while (val_ok.and.(ii.lt.precision(real_ref)))
+         i_test=floor(sci_val*10**ii,16)
+         i_ref=floor(sci_ref*10**ii,16)
+         
+         write(*,*)ii+1,i_ref,i_test,i_ref.eq.i_test
+         if ((i_ref.ne.i_test)) then
+            val_ok=.false.
+            write(*,'(1x,i2,a)')ii
+     $           ,' decimal places of precision achieved'
+            comp_real=ii
+            return
+         endif
+         ii=ii+1
+      enddo
+      if(val_ok) then
+         comp_real=precision(real_ref)
+      endif
+      write(*,*)ii
+      if(ii.eq.precision(real_ref))then
+         write(*,*)'yes'
+      else
+         write(*,*)'no'
+      endif
       end
