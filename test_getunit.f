34ceea5e4dc1da2a58f6c16f05aa8a99a11d72aa
Jon Lighthall <jon.lighthall@gmail.com>
2022-04-28 13:36:13 -0500

    fix/finish renamming

--- test_getunit.f
+++ test_getunit.f
@@ -1,13 +1,13 @@
       program test_getunit
       implicit none
-      integer lun,newunit
+      integer lun,getunit
       integer unit1,unit2,unit3,unit4
       
-      open(newunit(lun),file='test')
-      open(newunit(unit1),file='test1')
-      open(newunit(unit2),file='test2')
-      open(newunit(unit3),file='test3')
-      open(newunit(unit4),file='test4')
+      open(getunit(lun),file='test')
+      open(getunit(unit1),file='test1')
+      open(getunit(unit2),file='test2')
+      open(getunit(unit3),file='test3')
+      open(getunit(unit4),file='test4')
 
       close(lun)
       close(unit1)
