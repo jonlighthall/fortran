81ddd431d2b72e1199a6e748032297121e7932b9
Jon Lighthall <jon.lighthall@gmail.com>
2022-06-06 16:06:59 -0500

    add more examples

--- gethost.f
+++ gethost.f
@@ -1,9 +1,25 @@
       program gethost
-      character*32 hostname
-      call get_environment_variable('HOST',hostname)
-      write(*,*) 'My gracious host is ',trim(hostname)
+      character(len=256) hostname
+      integer length, status
+      logical trim
+      call get_environment_variable("HOST",hostname,length,status)
+      write(*,*) 'My gracious host is ',trim(hostname),length,status
       write(*,*) 'hostname length = ',len(trim(adjustl(hostname)))
-      call get_environment_variable('HOSTNAME',hostname)
-      write(*,*) 'My gracious host is ',trim(hostname)
+      call get_environment_variable("HOSTNAME",hostname,length,status)
+      write(*,*) 'My gracious host is ',trim(hostname),length,status
       write(*,*) 'hostname length = ',len(trim(adjustl(hostname)))
+     &     ,length,status
+
+      CALL getenv("HOME", hostname)
+      WRITE (*,*) TRIM(hostname)
+
+      CALL getenv("HOST", hostname)
+      WRITE (*,*) TRIM(hostname)
+
+      CALL getenv("HOSTNAME", hostname)
+      WRITE (*,*) TRIM(hostname)
+
+      call hostnm(hostname);
+      write(*,*) hostname
+
       end program gethost
