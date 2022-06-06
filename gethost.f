99dbd742c9179bab04dd55f812b34fb178af36d4
Jon Lighthall <jon.lighthall@gmail.com>
2022-06-06 16:13:23 -0500

    add nested hostname search

--- gethost.f
+++ gethost.f
@@ -3,23 +3,18 @@
       integer length, status
       logical trim
       call get_environment_variable("HOST",hostname,length,status)
-      write(*,*) 'My gracious host is ',trim(hostname),length,status
-      write(*,*) 'hostname length = ',len(trim(adjustl(hostname)))
-      call get_environment_variable("HOSTNAME",hostname,length,status)
-      write(*,*) 'My gracious host is ',trim(hostname),length,status
-      write(*,*) 'hostname length = ',len(trim(adjustl(hostname)))
-     &     ,length,status
-
-      CALL getenv("HOME", hostname)
+      if (length.eq.0) then
+         call get_environment_variable("HOSTNAME",hostname,length
+     &        ,status)
+         if (length.eq.0) then
+            CALL getenv("HOST", hostname)
+            if (len(trim(adjustl(hostname))).eq.0) then
+               CALL getenv("HOSTNAME", hostname)
+               if (len(trim(adjustl(hostname))).eq.0) then
+                  call hostnm(hostname);
+               endif
+            endif
+         endif
+      endif
       WRITE (*,*) TRIM(hostname)
-
-      CALL getenv("HOST", hostname)
-      WRITE (*,*) TRIM(hostname)
-
-      CALL getenv("HOSTNAME", hostname)
-      WRITE (*,*) TRIM(hostname)
-
-      call hostnm(hostname);
-      write(*,*) hostname
-
       end program gethost
