7e2e9d7cb68445c5a38e86431227a6d95cd2327f
Jon Lighthall <jon.lighthall@gmail.com>
2022-07-26 14:54:33 -0500

    clean up pause

--- pause.f
+++ pause.f
@@ -6,17 +6,14 @@
       do 10 i=1,3
          write(*,'(i2,a,a)')i,' beeps!'
      &        ,char(7)          ! print beeps at end of program
-      pause 'pause'
+! approximate PAUSE replacement; does not check past 2 characters
          write(*,'(a)') 'PAUSE'
          write(*,'(a)',advance="no") 'To resume execution, type go.  '
          write(*,'(a)') 'Other input will terminate the job.'
-         print *, 'pause (press enter to continue)'
          read(*,'(a)')words
          if(words=='go') then
-            print *,'yes, go'
             print '(a)','RESUMED'
          else
-            print *,'no go'
             exit
          endif
  10   continue
