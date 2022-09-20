fb31db66885b9bb581f4b4ee6a317b1da2fcc8b0
Jon Lighthall <jon.lighthall@gmail.com>
2022-09-20 15:51:46 -0500

    add module test

---
+++ inc/version.f
@@ -0,0 +1,12 @@
+      module version
+      implicit none
+      integer,parameter :: nspe_ver=42
+c     character(len=5) :: text='world'
+      character(len=9), parameter :: dayname(7) = [character(len=9) ::
+     &     'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+     &     'Saturday', 'Sunday' ]
+      contains
+      subroutine writedays()
+      write(*,*) dayname(1)
+      end subroutine writedays
+      end module version
---
+++ test_mod.f90
@@ -0,0 +1,7 @@
+      program hello
+      use version
+      implicit none
+      write(*,'(a)') "hello world"
+      write(*,'(a)') "fortan"
+      write(*,*) nspe_ver
+      end 
