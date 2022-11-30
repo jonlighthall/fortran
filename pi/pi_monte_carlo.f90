822300eb17aa2bd90fbdb2e5de9cbdf06cb0e9d8
Jon Lighthall <jon.lighthall@gmail.com>
2022-11-30 14:41:52 -0600

    add formatted output

--- pi/pi_monte_carlo.f90
+++ pi/pi_monte_carlo.f90
@@ -3,17 +3,16 @@ program pi_monte_carlo
   integer, parameter :: dp = 10
   integer,parameter :: srk = selected_real_kind(dp)
   real(kind=srk) :: x,y,error,pi_calculated,thresh
-  integer::n=0,square=0,circle = 0
+  integer::n=0,square=0,circle = 0,pdp=0,last=0
   real(kind=srk), parameter :: pi=3.141592653589793d0
-  real(kind=srk), parameter :: one=1,two=2,four=4
-  character(len=32) :: fmt1,fmt2
+  real(kind=srk), parameter :: one=1,two=2,four=4,ten=10
+  character(len=32) :: fmt1,fmt3,val,fmt4
   write(*,'(1x,i2,a,i2,a)')dp,' decimals reqested (using ',srk,' bytes)'
-  thresh=real(10.**(-dp),srk)
+  thresh=ten**(-dp)
   error=thresh+1
   write(fmt1,'("(a,f",i0,".",i0,")")')dp+2,dp
   print fmt1,'             pi = ',pi
   print fmt1,'error threshold = ',thresh
-  write(fmt2,'("(a,i10,2x,2(a,f",i0,".",i0,",2x))")')dp+2,dp
   do while (error>thresh)
      call random_number(x)
      call random_number(y)
@@ -23,10 +22,14 @@ program pi_monte_carlo
      square=square+1
      pi_calculated=four*(real(circle,srk)/real(square,srk))
      error=abs(pi_calculated-pi)
-     if(mod(n,int(1e6)).eq.0) then 
-          write(*,fmt2)"steps=",n,"pi=",pi_calculated,"error= ",error
+     pdp=(-ceiling(log10(error)))
+     if(pdp.gt.last) then
+        last=pdp
+        write(fmt3,'("(a,i10,2x,a,a",i0,",2x,a,f",i0,".",i0,")")')dp+2,dp+2,dp
+        write(fmt4,'("(f",i0,".",i0,")")')pdp+2-1,pdp-1
+        write(val,fmt4)pi_calculated
+        write(*,fmt3)"steps=",n,"pi=",val,"error= ",error
        endif
      n=n+1
   enddo
-  write(*,fmt2)"steps=",n,"pi=",pi_calculated,"error= ",error
 end program pi_monte_carlo
