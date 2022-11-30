f3264bf93ce0a6c9f2be653e173c4e49bcd1c985
Jon Lighthall <jon.lighthall@gmail.com>
2022-11-30 13:28:35 -0600

    add selected real kind

--- pi/pi_monte_carlo.f90
+++ pi/pi_monte_carlo.f90
@@ -1,23 +1,30 @@
 program pi_monte_carlo
   implicit none
-  double precision::x,y,error,pi_calculated,thresh
+  integer, parameter :: dp = 4
+  integer,parameter :: srk = selected_real_kind(dp)
+  real(kind=srk) :: x,y,error,pi_calculated,thresh
   integer::n=0,square=0,circle = 0
-  double precision, parameter::pi=3.141592653589793d0
-  thresh=1e-10
+  real(kind=srk), parameter :: pi=3.141592653589793
+  real(kind=srk), parameter :: one=1,two=2,four=4
+
+  write(*,'(1x,i2,a,i2,a)')dp,' decimals reqested (using ',srk,' bytes)'
+  thresh=real(10.**(-dp),srk)
   error=thresh+1
+  print*,'error threshold = ',thresh
   do while (error>thresh)
      call random_number(x)
      call random_number(y)
-     if (sqrt(x**2.+y**2.)<=1.) then
+     if (sqrt(x**two+y**two)<=one) then
         circle=circle+1
      end if
      square=square+1
-     pi_calculated=4.*(dble(circle)/dble(square))
+     pi_calculated=four*(real(circle,srk)/real(square,srk))
      error=abs(pi_calculated-pi)
-     if(mod(n,int(1e6)).eq.0) &
+     if(mod(n,int(1e6)).eq.0) then 
           write(*,101)"steps=",n,"pi=",pi_calculated,"error= ",error
+       endif
      n=n+1
-  end do
+  enddo
   print *, "finished"
   write(*,101)"steps=",n,"pi=",pi_calculated,"error= ",error
 101 format(a,i10,2x,2(a,f18.15,2x))
