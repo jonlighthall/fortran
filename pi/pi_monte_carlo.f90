734c7db83bd2dc75b252881c5495eff8f5faabe1
Jon Lighthall <jon.lighthall@gmail.com>
2022-11-30 12:56:28 -0600

    move ini to declare, make error abs

--- pi/pi_monte_carlo.f90
+++ pi/pi_monte_carlo.f90
@@ -1,13 +1,9 @@
 program pi_monte_carlo
   implicit none
-  double precision::x,y,error,pi_calculated
-  integer::n,square,circle
+  double precision::x,y,error=1,pi_calculated
+  integer::n=0,square=0,circle = 0
   double precision, parameter::pi=3.141592653589793d0
-  square=0
-  circle=0
-  error=1.
-  n=0
-  do while (abs(error)>1e-10)
+  do while (error>1e-10)
      call random_number(x)
      call random_number(y)
      if (sqrt(x**2.+y**2.)<=1.) then
@@ -15,7 +11,7 @@ program pi_monte_carlo
      end if
      square=square+1
      pi_calculated=4.*(dble(circle)/dble(square))
-     error=(pi_calculated-pi)
+     error=abs(pi_calculated-pi)
      if(mod(n,int(1e6)).eq.0) &
           write(*,101)"steps=",n,"pi=",pi_calculated,"error= ",error
      n=n+1
