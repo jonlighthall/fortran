68a87353f42673581083d7ae1ac0db5b189da459
Jon Lighthall <jon.lighthall@gmail.com>
2022-11-30 12:53:04 -0600

    working: add formatted print

--- pi/pi_monte_carlo.f90
+++ pi/pi_monte_carlo.f90
@@ -1,26 +1,26 @@
 program pi_monte_carlo
   implicit none
-  integer,parameter :: knd=8
-  real(kind=knd) :: x,y,error,pi_calculated
+  double precision::x,y,error,pi_calculated
   integer::n,square,circle
   double precision, parameter::pi=3.141592653589793d0
   square=0
   circle=0
-  error=real(1,knd)
+  error=1.
   n=0
-  do while (abs(error)>real(1e-10,knd))
+  do while (abs(error)>1e-10)
      call random_number(x)
      call random_number(y)
-     if (sqrt(x**real(2,knd)+y**real(2,knd))<=real(1,knd)) then
+     if (sqrt(x**2.+y**2.)<=1.) then
         circle=circle+1
      end if
      square=square+1
-     pi_calculated=real(4,knd)*(real(circle,knd)/real(square,knd))
+     pi_calculated=4.*(dble(circle)/dble(square))
      error=(pi_calculated-pi)
-     if(mod(n,int(1e5)).eq.0) &
-          write(*,*)"steps=",n,"pi=",pi_calculated,"error=",error
+     if(mod(n,int(1e6)).eq.0) &
+          write(*,101)"steps=",n,"pi=",pi_calculated,"error= ",error
      n=n+1
   end do
   print *, "finished"
-  write(*,*)"steps=",n,"pi=",pi_calculated,"error=",error
+  write(*,101)"steps=",n,"pi=",pi_calculated,"error= ",error
+101 format(a,i10,2x,2(a,f18.15,2x))
 end program pi_monte_carlo
