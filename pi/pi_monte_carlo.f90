297ee7be4a96674138276d6525ecf0c368486311
Jon Lighthall <jon.lighthall@gmail.com>
2022-11-30 11:58:17 -0600

    add kind variable to pi monte carlo

--- pi/pi_monte_carlo.f90
+++ pi/pi_monte_carlo.f90
@@ -1,22 +1,23 @@
 program pi_monte_carlo
   implicit none
-  double precision::x,y,error,pi_calculated
+  integer,parameter :: knd=8
+  real(kind=knd) :: x,y,error,pi_calculated
   integer::n,square,circle
   double precision, parameter::pi=3.141592653589793d0
   square=0
   circle=0
-  error=real(1,8)
+  error=real(1,knd)
   n=0
-  do while (abs(error)>real(1e-10,8))
+  do while (abs(error)>real(1e-10,knd))
      call random_number(x)
      call random_number(y)
-     if (sqrt(x**real(2,8)+y**real(2,8))<=real(1,8)) then
+     if (sqrt(x**real(2,knd)+y**real(2,knd))<=real(1,knd)) then
         circle=circle+1
         square=square+1
      else
         square=square+1
      end if
-     pi_calculated=real(4,8)*(dble(circle)/dble(square))
+     pi_calculated=real(4,knd)*(real(circle,knd)/real(square,knd))
      error=(pi_calculated-pi)
      if(mod(n,int(1e5)).eq.0) write(*,*)"steps=",n,"pi=",pi_calculated,"error=",error
      n=n+1
