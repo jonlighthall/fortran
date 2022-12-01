58dfb87f6d9bdff716c616391e668bdf3ccc40d7
Jon Lighthall <jon.lighthall@gmail.com>
2022-12-01 09:11:28 -0600

    add intermediate values

--- rounding.f90
+++ rounding.f90
@@ -1,6 +1,6 @@
 program rounding
   implicit none
-  integer i,n,m,o,p
+  integer i,n,m,o,p,ni,mi,oi
   integer, parameter :: mx = 100
   real a
   write(*,'(a)') "hello world"
@@ -9,7 +9,11 @@ program rounding
      n=amax1(0.,a) ! check value without adding 0.5
      m=amax1(0.,a+0.5) ! check value with 0.5
      o=max(0.,a+0.5) ! check equivalence of max
+     ni=int(amax1(0.,a)) ! check value without adding 0.5
+     mi=int(amax1(0.,a+0.5)) ! check value with 0.5
+     oi=int(max(0.,a+0.5)) ! check equivalence of max
+
      p=max(0,nint(a)) ! formulate without warnings
-     print '(i3,1x,f5.3,1x,5(i1,1x),l1)',i,a,int(a),n,m,o,p,(p.eq.m)
+     print '(i3,1x,f5.3,1x,9(i1,1x),l1)',i,a,int(a),nint(a),n,m,o,ni,mi,oi,p,(p.eq.m)
   enddo
 end program rounding
