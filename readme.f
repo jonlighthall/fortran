41a1dc65f8be40a86ed72999be0ee8fef64edb84
Jon Lighthall <jon.lighthall@gmail.com>
2023-08-14 17:17:44 -0500

    add 'readme' files with line length

--- readme.f
+++ readme.f
@@ -1,6 +1,14 @@
-c     fortran line length
+c     FORTRAN line length 72
 c     ...1....,....2....,....3....,....4....,....5....,....6....,....7..
 c     789012345678901234567890123456789012345678901234567890123456789012
 c     {----------------------------------------------------------------}
+c     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+c     eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+c     enim ad minim veniam, quis nostrud exercitation ullamco laboris
+c     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+c     reprehenderit in voluptate velit esse cillum dolore eu fugiat
+c     nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+c     sunt in culpa qui officia deserunt mollit anim id est laborum.
+
       program readme
       end
