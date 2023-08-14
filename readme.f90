0e59ac115515ac9cdc28058b0570683e582034ee
Jon Lighthall <jon.lighthall@gmail.com>
2023-08-14 17:17:44 -0500

    add 'readme' files with line length

---
+++ readme.f90
@@ -0,0 +1,20 @@
+! Fortran 90 free form 132
+!...,....+....,....+....,....+....,....+....,....+....,....+....,....+....,....+....,....+....,....1....,....+....,....+....,....+..
+!...,....1....,....2....,....3....,....4....,....5....,....6....,....7....,....8....,....9....,....0....,....1....,....2....,....3..
+!23456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012
+
+! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+! enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+! reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+! in culpa qui officia deserunt mollit anim id est laborum.
+
+! Fortran 90
+!--------------------Emacs PostScript "pretty-print" page width (97 columns)--------------------|
+!...,....1....,....2....,....3....,....4....,....5....,....6....,....7....,....8....,....9....,..
+!234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567
+
+! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+! labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+! laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+! voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+! non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
