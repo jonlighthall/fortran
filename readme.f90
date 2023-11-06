75755cf156736925874f8dcba41859a5d1e090b6
Jon Lighthall <jon.lighthall@gmail.com>
2023-11-06 14:15:52 -0600

    expand comments, add 80, 72; add print program

--- readme.f90
+++ readme.f90
@@ -1,23 +1,79 @@
-! Fortran 90 free form 132
-!...,....+....,....+....,....+....,....+....,....+....,....+....,....+....,....+....,....+....,....1....,....+....,....+....,....+..
-!...,....1....,....2....,....3....,....4....,....5....,....6....,....7....,....8....,....9....,....0....,....1....,....2....,....3..
-!23456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012
+! NB: column numbers start from 1.
 
+! ----------------------------------------------------------------------------------------------------------------------------------
+! Fortran 90 free form 132
+! Use the Fortran dialect compiler option -ffree-form or use the file extension .f90.
+! ..,....+....,....+....,....+....,....+....,....+....,....+....,....+....,....+....,....+....,....1....,....+....,....+....,....+..
+! ..,....1....,....2....,....3....,....4....,....5....,....6....,....7....,....8....,....9....,....0....,....1....,....2....,....3..
+! 3456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012
+! ----------------------------------------------------------------------------------------------------------------------------------
 ! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
 ! enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
 ! reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
 ! in culpa qui officia deserunt mollit anim id est laborum.
+! ----------------------------------------------------------------------------------------------------------------------------------
 
-! Fortran 90
-!--------------------Emacs PostScript "pretty-print" page width (97 columns)--------------------|
-!...,....1....,....2....,....3....,....4....,....5....,....6....,....7....,....8....,....9....,..
-!234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567
-
+! -----------------------------------------------------------------------------------------------
+! Fortran 90, 97 columns
+! Use the Fortran dialect compiler option -ffree-line-length-97
+! -------------------Emacs PostScript "pretty-print" page width (97 columns)---------------------
+! ..,....1....,....2....,....3....,....4....,....5....,....6....,....7....,....8....,....9....,..
+! 34567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567
+! -----------------------------------------------------------------------------------------------
 ! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 ! labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
 ! laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
 ! voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
 ! non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+! -----------------------------------------------------------------------------------------------
+
+! ------------------------------------------------------------------------------
+! Fortran 90, 80-column punch card
+! Use the Fortran dialect compiler option -ffree-line-length-80
+! ..,....1....,....2....,....3....,....4....,....5....,....6....,....7....,....8
+! 345678901234567890123456789012345678901234567890123456789012345678901234567890
+! ------------------------------------------------------------------------------
+! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+! incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+! nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+! consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+! cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+! proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+! ------------------------------------------------------------------------------
+
+! ----------------------------------------------------------------------
+! Fortran 90, 72-column line length
+! Use the Fortran dialect compiler option -ffixed-form or use the file
+! extension .f.
+! ..,....1....,....2....,....3....,....4....,....5....,....6....,....7..
+! 3456789012345678901234567890123456789012345678901234567890123456789012
+! ----------------------------------------------------------------------
+! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+! eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+! minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+! aliquip ex ea commodo consequat. Duis aute irure dolor in
+! reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+! pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+! culpa qui officia deserunt mollit anim id est laborum.
+! ----------------------------------------------------------------------
 
 program readme
+  implicit none
+  integer read_unit,ierr
+  character(len=132) :: line
+  open(newunit=read_unit,file='readme.f90',iostat=ierr,status='old',action='read')
+  if (ierr==0) then
+     print*,'OK'
+  else
+     print*,'ERROR'
+  endif
+  do
+     read(read_unit,'(a)',iostat=ierr) line
+     print*,trim(line)
+     if (ierr<0) then
+        print'(/a)','EOF'
+        exit
+     endif
+  enddo
+  close(read_unit)
 end program readme
