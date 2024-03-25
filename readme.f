c572e6722a3fb3d7d27bd113310c2086010489ae
Jon Lighthall <jon.lighthall@gmail.com>
2024-03-25 10:03:00 -0500

    make readme.f consistent with readme.f90

--- readme.f
+++ readme.f
@@ -1,4 +1,9 @@
-c     FORTRAN line length 72
+c     NB: column numbers start from 1.
+
+c     {----------------------------------------------------------------}
+c     FORTRAN 72-column line length
+c     Use the Fortran dialect compiler option -ffixed-form or use the
+c     file extension .f.
 c     ...1....,....2....,....3....,....4....,....5....,....6....,....7..
 c     789012345678901234567890123456789012345678901234567890123456789012
 c     {----------------------------------------------------------------}
@@ -9,6 +14,7 @@ c     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
 c     reprehenderit in voluptate velit esse cillum dolore eu fugiat
 c     nulla pariatur. Excepteur sint occaecat cupidatat non proident,
 c     sunt in culpa qui officia deserunt mollit anim id est laborum.
-
+c     {----------------------------------------------------------------}
+      
       program readme
       end
